caabbc1df5a865f7cdab8220ead77f4ecdddad3f
denisetroglio <overbeck.denise@gmail.com>
2022-01-13 11:16:17 +0100

    add input control

--- ahorcado/src/components/App.js
+++ ahorcado/src/components/App.js
@@ -1,17 +1,37 @@
-import { useState} from 'react';
+import { useState } from "react";
 import "../styles/App.scss";
-
 
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [lastLetter, setLastLetter] = useState("");
+  const [patata, setPatata] = useState("patata");
 
-const handleClickBtn = (ev) => {
-ev.preventDefault();
-console.log('estoy dentro');
-setNumberOfErrors(numberOfErrors + 1);
+  const [arrayInclude, setArrayInclude] = useState([]);
 
-}
+  const [arrayNotInclude, setArrayNotInclude] = useState([]);
 
+  const handleClickBtn = (ev) => {
+    ev.preventDefault();
+    console.log("estoy dentro");
+    setNumberOfErrors(numberOfErrors + 1);
+  };
+
+  const handleKeyUp = (ev) => {
+    const inputLetter = ev.currentTarget.value;
+    setLastLetter(inputLetter);
+    if (inputLetter != "") {
+      if (patata.includes(inputLetter)) {
+        arrayInclude.push(inputLetter);
+
+        //console.log(arrayInclude);
+        //console.log("la letra está");
+      } else {
+        arrayNotInclude.push(inputLetter);
+        console.log(arrayNotInclude);
+        console.log("la letra no está");
+      }
+    }
+  };
 
   return (
     <div className='page'>
@@ -55,26 +75,30 @@ setNumberOfErrors(numberOfErrors + 1);
               maxLength='1'
               type='text'
               name='last-letter'
-              id='last-letter' />
+              id='last-letter'
+              onChange={handleKeyUp}
+            />
           </form>
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
-        <span className='error-13 eye'></span>
-        <span className='error-12 eye'></span>
-        <span className='error-11 line'></span>
-        <span className='error-10 line'></span>
-        <span className='error-9 line'></span>
-        <span className='error-8 line'></span>
-        <span className='error-7 line'></span>
-        <span className='error-6 head'></span>
-        <span className='error-5 line'></span>
-        <span className='error-4 line'></span>
-        <span className='error-3 line'></span>
-        <span className='error-2 line'></span>
-        <span className='error-1 line'></span>
-      </section>
-    </main>
-    <button className='incrementarBtn' onClick={handleClickBtn}>Incrementar</button>
+          <span className='error-13 eye'></span>
+          <span className='error-12 eye'></span>
+          <span className='error-11 line'></span>
+          <span className='error-10 line'></span>
+          <span className='error-9 line'></span>
+          <span className='error-8 line'></span>
+          <span className='error-7 line'></span>
+          <span className='error-6 head'></span>
+          <span className='error-5 line'></span>
+          <span className='error-4 line'></span>
+          <span className='error-3 line'></span>
+          <span className='error-2 line'></span>
+          <span className='error-1 line'></span>
+        </section>
+      </main>
+      <button className='incrementarBtn' onClick={handleClickBtn}>
+        Incrementar
+      </button>
     </div>
   );
 }
