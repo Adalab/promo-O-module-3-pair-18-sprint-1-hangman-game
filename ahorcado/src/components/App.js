e55d2144cc7c9caa309cd49791162915c6c76462
pauladelavictoria <pauladelavictoria@gmail.com>
2022-01-17 11:27:00 +0100

    paint letters

--- ahorcado/src/components/App.js
+++ ahorcado/src/components/App.js
@@ -10,6 +10,8 @@ function App() {
 
   const [arrayNotInclude, setArrayNotInclude] = useState([]);
 
+  const keyWordArray = word.split("");
+
   const handleClickBtn = (ev) => {
     ev.preventDefault();
     console.log("estoy dentro");
@@ -18,89 +20,95 @@ function App() {
 
   const handleKeyUp = (ev) => {
     const inputLetter = ev.currentTarget.value;
+    if(inputLetter.match("^[a-z]?$")){
     setLastLetter(inputLetter);
-    if (inputLetter.match("^[a-z]?$")) {  //escribir las letras
-      setLastLetter(lastLetter);
-      if (word.includes(inputLetter)) {
-        //arrayInclude.push(inputLetter); //no se usa en react
-        setArrayInclude([...arrayInclude, inputLetter]); //"push" en react
+    if (keyWordArray.includes(inputLetter)) {
+      //arrayInclude.push(inputLetter); //no se usa en react
+      setArrayInclude([...arrayInclude, inputLetter]); //"push" en react
 
-
-        //console.log(arrayInclude);
-        //console.log("la letra está");
-      } else {
-        arrayNotInclude.push(inputLetter);
-        console.log(arrayNotInclude);
-        console.log("la letra no está");
-      }
+      //console.log(arrayInclude);
+      //console.log("la letra está");
+    } else {
+      setArrayNotInclude([...arrayNotInclude, inputLetter]);
+      console.log(arrayNotInclude);
+      console.log("la letra no está");
     }
+  }
   };
 
+  // const renderSolutionLetters = () => {
+  //   escribir las letras
+  //   keyWordArray.map((eachLetter, index) => {
+  //     return (
+  //       <li key={index} className="letter">
+  //         {eachLetter}
+  //       </li>
+  //     );
+  //   });
+  // };
+
   return (
-    <div className='page'>
+    <div className="page">
       <header>
-        <h1 className='header__title'>Juego del ahorcado</h1>
+        <h1 className="header__title">Juego del ahorcado</h1>
       </header>
-      <main className='main'>
+      <main className="main">
         <section>
-          <div className='solution'>
-            <h2 className='title'>Solución:</h2>
-            <ul className='letters'>
-              <li className='letter'>k</li>
-              <li className='letter'>a</li>
-              <li className='letter'></li>
-              <li className='letter'>a</li>
-              <li className='letter'>k</li>
-              <li className='letter'>r</li>
-              <li className='letter'></li>
-              <li className='letter'>k</li>
-              <li className='letter'>e</li>
-              <li className='letter'>r</li>
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+            <ul className="letters">{arrayInclude.map((eachLetter, index) => {
+                return (
+                  <li key={index} className="letter">
+                    {eachLetter}
+                  </li>
+                );
+              })}</ul>
+          </div>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">
+              {arrayNotInclude.map((eachLetter, index) => {
+                return (
+                  <li key={index} className="letter">
+                    {eachLetter}
+                  </li>
+                );
+              })}
             </ul>
           </div>
-          <div className='error'>
-            <h2 className='title'>Letras falladas:</h2>
-            <ul className='letters'>
-              <li className='letter'>f</li>
-              <li className='letter'>q</li>
-              <li className='letter'>h</li>
-              <li className='letter'>p</li>
-              <li className='letter'>x</li>
-            </ul>
-          </div>
-          <form className='form'>
-            <label className='title' htmlFor='last-letter'>
+          <form className="form">
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
-              autoComplete='off'
-              className='form__input'
-              maxLength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
+              autoComplete="off"
+              className="form__input"
+              maxLength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
               onChange={handleKeyUp}
               value={lastLetter}
             />
           </form>
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
-          <span className='error-13 eye'></span>
-          <span className='error-12 eye'></span>
-          <span className='error-11 line'></span>
-          <span className='error-10 line'></span>
-          <span className='error-9 line'></span>
-          <span className='error-8 line'></span>
-          <span className='error-7 line'></span>
-          <span className='error-6 head'></span>
-          <span className='error-5 line'></span>
-          <span className='error-4 line'></span>
-          <span className='error-3 line'></span>
-          <span className='error-2 line'></span>
-          <span className='error-1 line'></span>
+          <span className="error-13 eye"></span>
+          <span className="error-12 eye"></span>
+          <span className="error-11 line"></span>
+          <span className="error-10 line"></span>
+          <span className="error-9 line"></span>
+          <span className="error-8 line"></span>
+          <span className="error-7 line"></span>
+          <span className="error-6 head"></span>
+          <span className="error-5 line"></span>
+          <span className="error-4 line"></span>
+          <span className="error-3 line"></span>
+          <span className="error-2 line"></span>
+          <span className="error-1 line"></span>
         </section>
       </main>
-      <button className='incrementarBtn' onClick={handleClickBtn}>
+      <button className="incrementarBtn" onClick={handleClickBtn}>
         Incrementar
       </button>
     </div>
